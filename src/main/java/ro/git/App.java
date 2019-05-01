1cbab8d14565b94dde081147de224165c94dd23a
develoe <muller_klaus@yahoo.com>
2019-04-30 17:16:51 -0700

    this is a Four Commit

--- src/main/java/ro/git/App.java
+++ src/main/java/ro/git/App.java
@@ -10,6 +10,10 @@ public class App
     {
         //TODO Create a app Home Inteligents
         //TODO Create A Class Home
+        Home home = new Home("Nechiforel", true);
+        home.setMetriPatrati(home.calculeazaM4(10.5,7.8));
+        System.out.printf("%.2f",home.getMetriPatrati());
+
 
 
 
--- src/main/java/ro/git/Home.java
+++ src/main/java/ro/git/Home.java
@@ -6,4 +6,27 @@ public class Home {
     private double metriPatrati;
     private boolean casaPasiva;
     private boolean bransamentGaz;
+    // TODO Acesta nu este considerat fiind mebru al clasei
+    public Home(String numeCasa , boolean casaPasiva){
+        this.numeCasa = numeCasa;
+        this.casaPasiva = casaPasiva;
+    }
+    public boolean izoleazaCasa(){
+        return casaPasiva == false ? false : true;
+    }
+    public double calculeazaM4(double lungime,double latime){
+        return lungime*latime;
+    }
+
+    public void setMetriPatrati(double metriPatrati) {
+        this.metriPatrati = metriPatrati;
+    }
+    public double getMetriPatrati(){
+        return this.metriPatrati;
+    }
+
+    @Override
+    public String toString() {
+        return "Casa pe care ati apelat-o se cheama " + this.numeCasa + " metriPatrati  = " + this.metriPatrati;
+    }
 }
