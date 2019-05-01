d5e282173fda2fd5bf2c7d5a9da04416437cc5c6
develoe <muller_klaus@yahoo.com>
2019-05-01 10:15:33 -0700

    this is a Fx Start Commit

--- src/main/java/ro/git/App.java
+++ src/main/java/ro/git/App.java
@@ -1,11 +1,21 @@
 package ro.git;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import javax.lang.model.element.NestingKind;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
+public class App extends Application implements EventHandler<ActionEvent>
+{   private Button button;
     public static void main( String[] args )
     {
         //TODO Create a app Home Inteligents
@@ -13,9 +23,33 @@ public class App
         Home home = new Home("Nechiforel", true);
         home.setMetriPatrati(home.calculeazaM4(10.5,7.8));
         System.out.printf("%.2f",home.getMetriPatrati());
+        launch(args);
 
 
 
+    }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("This is the Best Application");
+        button = new Button();
+        button.setText("Pres Me");
+        button.setOnAction(this);
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+
+
+        Scene scene = new Scene(layout,400,300);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    @Override
+    public void handle(ActionEvent event) {
+        if (event.getSource()==button){
+            System.out.println("ce faci");
+        }
     }
 }
